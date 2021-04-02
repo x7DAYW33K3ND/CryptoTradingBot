cad1b4e63645049e1d284f163f06a5f1a67dac24
JS <greycolorhunter@gmail.com>
2021-04-02 15:16:35 -0400

    js stuff

--- LoginPage/index.js
+++ LoginPage/index.js
@@ -2,3 +2,9 @@
 function logInF(){
     window.open("..\\MainPage\\MainPage.html");
 }
+
+function checkpsw(psw, repsw){
+    if (psw == repsw){
+        return true;
+    }
+}
--- LoginPage/userclass.js
+++ LoginPage/userclass.js
@@ -1,1 +1,16 @@
 // Creating user class
+class User{
+    
+    constructor(email, psw, bkey, bskey){
+        this.email = email;
+        this.psw = psw;
+        this.bkey = bkey;
+        this.bskey = bskey;
+
+    }
+
+
+
+
+
+}
