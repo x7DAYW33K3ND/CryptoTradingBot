227e45f6970ce76cc8484368c5f5a5b982fa75ec
JS <greycolorhunter@gmail.com>
2021-04-02 16:08:34 -0400

    starting a js class

--- LoginPage/index.js
+++ LoginPage/index.js
@@ -3,8 +3,14 @@ function logInF(){
     window.open("..\\MainPage\\MainPage.html");
 }
 
+// Checking to make sure passwords match
 function checkpsw(psw, repsw){
     if (psw == repsw){
         return true;
     }
 }
+
+// Makes sure everything in the form is loaded before submitting.
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', addUser)
+});
