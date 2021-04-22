c99d5ad73cbd0712f33ac0c882d51728c0369f73
Nathan Colosimo <63754748+nathan-colosimo@users.noreply.github.com>
2021-04-22 02:18:44 -0400

    Working "Logout"

--- MainPage/main.js
+++ MainPage/main.js
@@ -1,20 +1,7 @@
 class Main {
-//https://tuhrig.de/basic-auth-log-out-with-javascript/
+//Temporary
   function logOut() {
-    jQuery.ajax({
-      type: "GET",
-      url: "/ui",
-      async: false,
-      username: "logmeoutplease",
-      password: "123alphabet123",
-      headers: {"Authorization": "Basic xxxx" }
-    })
-  .done(function() {
-      // logOut() should fail as a 401
-    })
-  .fail(function() {
-      window.location = "/ui.html";
-   });
- return false;   
+  window.open("ui.html");
+	window.close("MainPage.html");
  }
 }
