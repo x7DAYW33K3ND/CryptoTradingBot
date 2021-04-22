2f5d7a3f1dde6231fb9978d10d67069b7a856e0c
JS <greycolorhunter@gmail.com>
2021-04-21 22:24:31 -0400

    Added user creation and login funtionality

--- LoginPage/index.js
+++ LoginPage/index.js
@@ -3,8 +3,13 @@
  */
 
 // Creating user class
-// Document()
-var userArray = [];
+
+
+
+
+// var userArray = [];
+
+var userArray = []
 class User {
 
     constructor(email, psw, key, skey) {
@@ -20,7 +25,7 @@ class User {
     }
 
     getPassword(){
-        return this.password;
+        return this.psw;
     }
 
     getKey(){
@@ -39,22 +44,46 @@ class User {
         this.password = this.password;
     }
 
-
 }
 
+// Creating user for testing
+var user0 = new User('user0', 'pass', 'key', 'secret key');
+var user1 = new User('user1', 'pass', 'key', 'secret key');
+var user2 = new User('user2', 'pass', 'key', 'secret key');
+
+// Adding user to array for testing
+userArray.push(user0);
+userArray.push(user1);
+userArray.push(user2);
 
 
+function logIn() {
+    var enteredEmail = document.getElementById('loginEmail').value;
+    var enteredPass = document.getElementById('loginPass').value;
+    enteredEmail = enteredEmail.toString();
+    enteredPass = enteredPass.toString();
 
-function logInF() {
-    window.open("..\\MainPage\\MainPage.html");
-    // window.location.replace("..\\MainPage\\MainPage.html");
+    console.log('attempting login');
+    // var index = 0;
+    for (i = 0; i < userArray.length; i++){
+        console.log("loop started");
+        if(userArray[i].getEmail() == enteredEmail){
+            console.log('correct email');
+            if(userArray[i].getPassword() == enteredPass){
+                console.log('your in');
+                window.open("..\\MainPage\\MainPage.html");
+            }else{
+                alert("Password does not match")
+            }
+        }
+    }
+    
 }
 
 
 function checkPass(){
     var password = document.querySelector('.password').value,
     confirmPassword =  document.querySelector('.confirmPassword').value;
-
         if(password != confirmPassword){
             alert("Passwords don't match try again.");
             return false;
@@ -62,7 +91,17 @@ function checkPass(){
         else {
             return true;
         }
+}
 
+function checkDupAcount(newEmail) {
+    
+    for (i = 0; i < userArray.length; i++){
+        if(userArray[i].getEmail() == newEmail){
+            alert("Account with Email already exists");
+            return false
+        }
+    }
+    return true;
 }
 
 
@@ -78,29 +117,66 @@ function checkPass(){
 
 // What you need to do is get these before the funtion is called
 function createUser() {
-    console.log('getting hit');
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var apikey = document.getElementById("api-key").value;
     var sapikey = document.getElementById("secret-api-key").value;
-    var user = new User(email, password, apikey, sapikey);
-    userArray.push(user);
+    
+    // Validates there are no null fields
+    if(email == '' || password == '' || apikey == '' || sapikey == ''){
+        alert("Please fill all fields in");
+        return;
+    }
 
-    if (checkPass()){
-        var user = {email:email, psw:password, key:apikey, skey:sapikey};
-        // var user = new User(email, password, apikey, sapikey);
+    var user = new User(email, password, apikey, sapikey);
+
+    // Makes sure passwords are the same and there is no other created account with the same email
+    if (checkPass() && checkDupAcount(email)){
         userArray.push(user);
-        
+        console.log('User Created');
     }
 
 
 }
 
+// function two() {
+//     createUser();
+//     savetoFile();
+// }
 function thisWasClicked() {
     console.log('This was clicked');
 }
 
+// function savetoFile(){
+//     const fs = require('fs');
 
+//     var data = JSON.stringify(userArray, null, 2);
+
+//     fs.writeFileSync('users.json', data, (err) => {
+//         if (err){
+//             throw err;
+//         }
+//         console.log("Saved.");
+//     });
+
+// }
+
+// function loadFile(){
+//     const fs = require('fs');
+
+//     fs.readFileSync('users.json', 'utf-8', (err,data) => {
+//         if (err){
+//             throw err;
+//         } else {
+//             try{
+//                 const file = JSON.parse(data);
+//                 userArray.users.push(file);
+//             } catch (error){
+//                 console.error(error);
+//             }
+//         }
+//     });
+// }
 /**
  * Makes sure everything in the form is loaded before submitting.
  */
